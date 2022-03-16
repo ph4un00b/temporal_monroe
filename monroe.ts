8d9ddc595143b7349686e43006a2bcc2a476ffa1
ph4u <faunotv@gmail.com>
2022-03-16 11:59:25 -0600

    chore(Root): Avoid request /getMe endpoint by caching data.

--- monroe.ts
+++ monroe.ts
@@ -1,9 +1,29 @@
 import { Bot } from "https://deno.land/x/grammy/mod.ts";
-import { config } from "https://deno.land/x/dotenv/mod.ts";
+// import { config } from "https://deno.land/x/dotenv/mod.ts";
 // config({ safe: true, export: true });
+
 const token = Deno.env.get("TOKEN");
+const id = Number(Deno.env.get("BOT_ID"));
+const name = Deno.env.get("BOT_NAME");
+const username = Deno.env.get("BOT_USERNAME");
+
 if (typeof token !== "string") throw new Error("no token!");
-export const bot = new Bot(token);
+if (typeof id !== "number") throw new Error("no bot id!");
+if (typeof name !== "string") throw new Error("no bot name!");
+if (typeof username !== "string") throw new Error("no bot username!");
+
+export const bot = new Bot(token, {
+  // https://api.telegram.org/bot<bot_token>/getMe
+  botInfo: {
+    id: id,
+    is_bot: true,
+    first_name: name,
+    username,
+    can_join_groups: true,
+    can_read_all_group_messages: false,
+    supports_inline_queries: false,
+  },
+});
 
 bot.command("start", (ctx) => ctx.reply("monroe: Welcome! Send me a b0tnude!"));
 
